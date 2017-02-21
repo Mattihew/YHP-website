38bf4f496c9b949b8508989232b833d8c445d5c5
Matt Rayner <me@mattihew.com>
2017-02-21 20:21:42 +0000

    added example classes

---
+++ YHP_website/src/test/java/com/example/servlets/ExampleTest.java
@@ -0,0 +1,15 @@
+package com.example.servlets;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest
+{
+
+	@Test
+	public void testTest()
+	{
+		Assert.assertEquals(ExampleServlet.test(), "Test");
+	}
+
+}
