a038c781d3c6175b437bad376fb616b6266ac558
Matt Rayner <me@mattihew.com>
2017-02-24 18:20:43 +0000

    moved database code to tests

--- YHP_website/src/main/java/com/example/servlets/ExampleServlet.java
+++ YHP_website/src/main/java/com/example/servlets/ExampleServlet.java
@@ -1,19 +1,11 @@
 package com.example.servlets;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 
 /**
  * Servlet implementation class ExampleServlet
@@ -36,32 +28,5 @@ public class ExampleServlet extends HttpServlet {
 	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		try
-		{
-			Context init = new InitialContext();
-			DataSource ds = (DataSource) init.lookup("java:/comp/env/jdbc/postgres");
-			Connection con = ds.getConnection();
-			Statement stat = con.createStatement();
-			ResultSet rs = stat.executeQuery("select * from test");
-			while (rs.next())
-			{
-				System.out.println(rs.getInt(2));
-			}
-			con.close();
-		} catch (NamingException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
-	
-	public static String test()
-	{
-		return "Test";
-	}
-
 }
--- YHP_website/src/test/java/com/example/servlets/ExampleTest.java
+++ YHP_website/src/test/java/com/example/servlets/ExampleTest.java
@@ -1,5 +1,15 @@
 package com.example.servlets;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,7 +20,29 @@ public class ExampleTest
 	@Test
 	public void testTest()
 	{
-		Assert.assertThat(ExampleServlet.test(), CoreMatchers.is("Test"));
+		Integer number = null;
+		try
+		{
+			Context init = new InitialContext();
+			DataSource ds = (DataSource) init.lookup("java:/comp/env/jdbc/postgres");
+			Connection con = ds.getConnection();
+			Statement stat = con.createStatement();
+			ResultSet rs = stat.executeQuery("select * from test");
+			if(rs.next())
+			{
+				number = rs.getInt(2);
+			}
+			con.close();
+		} catch (NamingException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertThat(number, CoreMatchers.is(Integer.valueOf(1)));
 	}
 
 }
