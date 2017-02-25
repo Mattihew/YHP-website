d16b08bfbf5c8b6bb29a200cec1e079d534afe57
jsjlewis96 <jsjlewis96@gmail.com>
2017-02-25 16:09:22 +0000

    Account Management
    Added inital address support for User. Adding logic for Address class

--- YHP_website/src/main/java/models/Address.java
+++ YHP_website/src/main/java/models/Address.java
@@ -2,10 +2,21 @@ package models;
 
 public class Address
 {
+	private String building;
+	private String street;
+	private String city_town;
+	private String area_code;
+	private String country;
+	
+	private Address()
+	{
+		
+	}
+	
 	/**
 	 * Class Constructor.
 	 */
-	public Address()
+	public Address(String, String, String, String, String)
 	{
 		super();
 	}
--- YHP_website/src/main/java/models/User.java
+++ YHP_website/src/main/java/models/User.java
@@ -2,11 +2,29 @@ package models;
 
 public class User extends Person
 {
+	
+	private Address address;
+	
 	/**
 	 * Class Constructor.
 	 */
 	public User(String forename, String surname)
 	{
 		super(forename, surname);
+	}
+	
+	/**
+	 * Class Constructor.
+	 */
+	public User(String forename, String surname, Address address)
+	{
+		super(forename, surname);
+		
+		this.address = address;
+	}
+	
+	public void addAddress()
+	{
+		//stub
 	}
 }
