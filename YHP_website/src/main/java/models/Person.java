be2d48872f7ea5e608dbe134c67b89257c7da9ef
Matt Rayner <me@mattihew.com>
2017-02-27 22:47:08 +0000

    #15: added userRoles to User class. also added user builder

--- YHP_website/src/main/java/models/Person.java
+++ YHP_website/src/main/java/models/Person.java
@@ -8,16 +8,21 @@ public abstract class Person
 	private final String forename;
 	private final String surname;
 
-	public Person(String forename, String surname)
+	public Person(final String forename, final String surname)
 	{
 		this(UUID.randomUUID(), forename, surname);
 	}
 
-	public Person(UUID uuid, String forename, String surname)
+	public Person(final UUID uuid, final String forename, final String surname)
 	{
 		this.id = uuid;
 		this.forename = forename;
 		this.surname = surname;
+	}
+	
+	protected Person(final Builder builder)
+	{
+		this(builder.id, builder.forename, builder.surname);
 	}
 
 	/**
@@ -25,7 +30,7 @@ public abstract class Person
 	 */
 	public UUID getUuid()
 	{
-		return id;
+		return this.id;
 	}
 
 	/**
@@ -33,7 +38,7 @@ public abstract class Person
 	 */
 	public String getForename()
 	{
-		return forename;
+		return this.forename;
 	}
 
 	/**
@@ -41,6 +46,31 @@ public abstract class Person
 	 */
 	public String getSurname()
 	{
-		return surname;
+		return this.surname;
+	}
+	
+	public abstract static class Builder
+	{
+		private UUID id = UUID.randomUUID();
+		private String forename = "";
+		private String surname = "";
+		
+		public Builder id(final UUID value)
+		{
+			this.id = value;
+			return this;
+		}
+		
+		public Builder forename(final String value)
+		{
+			this.forename = value;
+			return this;
+		}
+		
+		public Builder surname(final String value)
+		{
+			this.surname = value;
+			return this;
+		}
 	}
 }
--- YHP_website/src/main/java/models/User.java
+++ YHP_website/src/main/java/models/User.java
@@ -3,28 +3,77 @@ package models;
 public class User extends Person
 {
 	
-	private Address address;
+	private final Address address;
+	
+	private final UserRole role;
 	
 	/**
 	 * Class Constructor.
 	 */
-	public User(String forename, String surname)
+	public User(final String forename, final String surname)
 	{
-		this(forename, surname, null);
+		this(forename, surname, UserRole.getDefault());
+	}
+	
+	/**
+	 * Class Constructor.
+	 *
+	 * @param forename
+	 * @param surname
+	 * @param userRole
+	 */
+	public User(final String forename, final String surname, final UserRole userRole)
+	{
+		this(forename, surname, userRole, Address.NULL);
 	}
 	
 	/**
 	 * Class Constructor.
 	 */
-	public User(String forename, String surname, Address address)
+	public User(final String forename, final String surname, final UserRole userRole, final Address address)
 	{
 		super(forename, surname);
-		
 		this.address = address;
+		this.role = userRole;
+	}
+	
+	protected User(final Builder builder)
+	{
+		super(builder);
+		this.address = builder.address;
+		this.role = builder.role;
 	}
 	
 	public Address getAddress()
 	{
 		return this.address;
+	}
+	
+	public UserRole getRole()
+	{
+		return this.role;
+	}
+	
+	public static class Builder extends Person.Builder
+	{
+		private Address address = Address.NULL;
+		private UserRole role = UserRole.getDefault();
+		
+		public Builder address(final Address value)
+		{
+			this.address = value;
+			return this;
+		}
+		
+		public Builder userRole(final UserRole value)
+		{
+			this.role = value;
+			return this;
+		}
+		
+		public User build()
+		{
+			return new User(this);
+		}
 	}
 }
