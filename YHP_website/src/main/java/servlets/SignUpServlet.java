4a66c6693466b53a1458eb78784ac3696cb053a9
jsjlewis96 <jsjlewis96@gmail.com>
2017-03-01 16:59:24 +0000

    #7 Logout
    Added logout functionality and stubs for sign up

---
+++ YHP_website/src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignUpServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Class Constructor.
+	 * 
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SignUpServlet()
+	{
+		super();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see HttpServlet#doGet(HttpServletRequest, HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException
+	{
+		//request.getRequestDispatcher("SignUp");
+		response.sendError(418, "Teapot not found.");
+	}
+}
