022180c9d9e6d48072654536436a44dcbb4fd33b
Matt Rayner <me@mattihew.com>
2017-03-01 19:31:21 +0000

    #17: made user cache a singleton

--- YHP_website/src/main/java/storage/UserCache.java
+++ YHP_website/src/main/java/storage/UserCache.java
@@ -2,14 +2,11 @@ package storage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import org.apache.commons.dbutils.BaseResultSetHandler;
-import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.AbstractListHandler;
 import org.apache.commons.dbutils.handlers.ColumnListHandler;
@@ -19,25 +16,37 @@ import models.UserRole;
 
 public class UserCache
 {
+	private static UserCache INSTANCE;
+	
 	private final Database database;
 	
 	private final Map<UUID, User> users = new HashMap<>();
+	
+	public UserCache getInstance()
+	{
+		if (UserCache.INSTANCE == null)
+		{
+			UserCache.INSTANCE = new UserCache(Database.getInstance());
+		}
+		return UserCache.INSTANCE;
+	}
 	
 	public UserCache(final Database database)
 	{
 		this.database = database;
 	}
 	
+	
 	public User getUser(final String username)
 	{
 		QueryRunner run = new QueryRunner(Database.getInstance().getDataSource());
-		List<User> users = null;;
+		List<User> users = null;
 		try
 		{
 			users = run.query(
 					"SELECT * FROM users WHERE user_name='" + username + "';",
 					new UserResultsSetHandler());
-		} 
+		}
 		catch (SQLException e)
 		{
 			e.printStackTrace();
@@ -58,9 +67,9 @@ public class UserCache
 	private class UserResultsSetHandler extends AbstractListHandler<User>
 	{
 		@Override
-		protected User handleRow(ResultSet rs) throws SQLException
+		protected User handleRow(final ResultSet rs) throws SQLException
 		{
-			final User.Builder userBuilder = 
+			final User.Builder userBuilder =
 					new User.Builder(rs.getString("forename"), rs.getString("surname"));
 			final QueryRunner roleRunner = new QueryRunner(Database.getInstance().getDataSource());
 			final List<String> roles = roleRunner.query(
