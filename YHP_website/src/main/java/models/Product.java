b21c57fd521a6e31a39e1090672fdf7c3807098c
Matt Rayner <me@mattihew.com>
2017-03-04 20:06:54 +0000

    #42: added product and test

--- YHP_website/src/main/java/models/Product.java
+++ YHP_website/src/main/java/models/Product.java
@@ -2,11 +2,85 @@ package models;
 
 public class Product
 {
+	private final String name;
+	
+	private final String desc;
+	
+	private final String type;
+	
+	private final int price;
+	
+	private final int quantity;
+	
+	private final String imageURL;
 	/**
 	 * Class Constructor.
 	 */
-	public Product()
+	public Product(final String name, final String desc, final String type, final int price, final int quantity, final String imageURL)
 	{
 		super();
+		this.name = name;
+		this.desc = desc;
+		this.type = type;
+		this.price = price;
+		this.quantity = quantity;
+		this.imageURL = imageURL;
 	}
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+	/**
+	 * @return the desc
+	 */
+	public String getDescription()
+	{
+		return this.desc;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType()
+	{
+		return this.type;
+	}
+	/**
+	 * @return the price
+	 */
+	public int getPricePennys()
+	{
+		return this.price;
+	}
+	
+	public String getPriceString()
+	{
+		String result = Integer.toString(this.price);
+		if (result.length() < 3)
+		{
+			result = result + "p";
+		}
+		else
+		{
+			result = "£" + result.substring(0, result.length() - 2) + "." + result.substring(result.length() - 2);
+		}
+		return result;
+	}
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity()
+	{
+		return this.quantity;
+	}
+	/**
+	 * @return the imageURL
+	 */
+	public String getImageURL()
+	{
+		return this.imageURL;
+	}
+	
 }
